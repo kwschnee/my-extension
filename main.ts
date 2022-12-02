42c7ca0f9acfbdc8a501e231659f3905c0145b60
kwschnee <118218454+kwschnee@users.noreply.github.com>
2022-12-02 12:51:49 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+scene.setBackgroundColor(10)
+effects.starField.startScreenEffect()
+pause(3000)
+game.setDialogFrame(img`
+    ...cc......................cc....
+    ..c55c..bbbb...bbbbb......c55c...
+    .cb55bcbdddbbbbbdddbbbbbbcb55bc..
+    b555555bbdddb111bdddb11db555555b.
+    bb5555bbdbdb11111bdb1111bb5555bb.
+    cb5555bcddd11111ddd11111cb5555bc.
+    .c5bb5c1111d111d111d111ddc5bb5c..
+    .cbbbbc111111111111111111cbbbbc..
+    ..b11111111111111111111111d111bb.
+    ..b111111111111111111111111d1bdb.
+    ..bb11111111111111111111111dbddb.
+    .bbdb1d11111111111111111111ddddb.
+    .bdddd11111111111111111111d1bdbb.
+    .bddbd11111111111111111111111bb..
+    .bdb1d111111111111111111111111b..
+    .bb111d11111111111111111111111b..
+    ..b11111111111111111111111d111bb.
+    ..b111111111111111111111111d1bdb.
+    ..bb11111111111111111111111dbddb.
+    .bbdb1d11111111111111111111ddddb.
+    .bdddd11111111111111111111d1bdbb.
+    .bddbd11111111111111111111111bb..
+    .bdbb1111111111111111111111111b..
+    .bbbd1111111111111111111111111b..
+    ..bcc111111111111111111111dccdb..
+    ..c55c111d111d111d111d1111c55cb..
+    .cb55bcdd11111ddd11111dddcb55bc..
+    b555555b11111bdb11111bdbb555555b.
+    bb5555bbb111bdddb111bdddbb5555bb.
+    cb5555bcdbbbbbdddbbbbbddcb5555bc.
+    .c5bb5c.bb...bbbbb...bbbbc5bb5c..
+    .cbbbbc..................cbbbbc..
+    .................................
+    `)
+game.setDialogTextColor(15)
+game.showLongText("Hello", DialogLayout.Center)
+effects.starField.endScreenEffect()
+music.magicWand.play()
